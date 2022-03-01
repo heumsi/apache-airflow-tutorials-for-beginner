b97789fdee99984236c5d202aa7732264cb1ae31
heumsi <heumsi@naver.com>
2022-03-01 22:26:42 +0900

    feat(docs/src/.vuepress/config.js): ga와 gsc를 추가한다

--- docs/src/.vuepress/config.js
+++ docs/src/.vuepress/config.js
@@ -20,7 +20,16 @@ module.exports = {
   head: [
     ['meta', { name: 'theme-color', content: '#3eaf7c' }],
     ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
-    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ['meta', { name: 'google-site-verification', content: 'wjX_mSoZBgO9SZMvjr96yOjo6n3_7pS8xNdmzDl1ESw' }],
+    [
+      "script",
+      {
+        async: true,
+        src: "https://www.googletagmanager.com/gtag/js?id=G-PW03QPB3R3",
+      },
+    ],
+    ["script", {}, ["window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-PW03QPB3R3');"]],
   ],
 
   /**
